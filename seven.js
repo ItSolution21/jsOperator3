34708904aa38c4104b460c06815d2f9b4c64f602
ItSolution21 <161233126+ItSolution21@users.noreply.github.com>
2024-03-08 13:11:21 +0530

    Add files via upload

---
+++ seven.js
@@ -0,0 +1,38 @@
+
+//bitwise operator 
+
+let a = 7;
+let b = 3;
+
+console.log((a | b )| 14);
+
+// 5 -> 101 
+// 2 -> 010
+// & -> 000 -> 0
+
+// 64 32 16 8 4 2 1 
+           
+// 7 -> 111
+// 3 -> 011
+
+// | -> 111
+// 7  -> 0111
+// 12 -> 1100
+// |  -> 1111
+
+// 7->  0111
+// 14-> 1110
+// | -> 1111-> 15
+
+// condition statements 
+
+if(!false){
+    console.log("Program ...")
+}
+
+if( false){
+    console.log("Program.....")
+}
+else{
+    console.log("Hello world...");
+}
